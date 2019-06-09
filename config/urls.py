2cbb1c597335b78a00018855b9bd4883ab37c4bd
kaajavi <javierguignard@gmail.com>
2019-06-09 16:53:53 -0300

    Change urls.py (admin and jet in the same path)

--- config/urls.py
+++ config/urls.py
@@ -24,7 +24,7 @@ urlpatterns = [
     path('', include('sells.urls')),
     path('', include('production.urls')),
     path('', include('accounts.urls')),
-    path(r'jet/', include('jet.urls', 'jet')),
+    path(r'admin/jet/', include('jet.urls', 'jet')),
     path('admin/', admin.site.urls),
 
 ]
